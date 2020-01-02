2cfbda2cae13b43f5022d5d01a65f4829b68fc27
Xanonymous <trusaidlin@gmail.com>
2020-01-02 17:28:38 +0800

    init kayout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-app justify-center>
+    <v-container fill-height>
+      <v-card
+        bg
+        fill-height
+        grid-list-md
+        text-xs-center
+        row
+        wrap
+        align-center
+        class="mx-auto"
+        max-width="600"
+        justify-center
+        justify="center"
+      >
+        <mainImage />
+
+        <v-card-subtitle class="pb-0">Xanonymous</v-card-subtitle>
+
+        <v-card-text class="text--primary">
+          <div>按下按鈕，神奇的事情將會發生。</div>
+
+          <div>純屬娛樂，請勿自行嘗試。</div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn color="orange" text>
+            Share
+          </v-btn>
+
+          <v-btn color="orange" text>
+            Explore
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+const mainImage = ()=>import("@/components/mainImage");
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  components:{
+    mainImage
   }
 };
 </script>
