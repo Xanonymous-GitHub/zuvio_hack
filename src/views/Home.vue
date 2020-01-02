665454be84854c715d7928a5c03e6be4a1caa5b2
Xanonymous <trusaidlin@gmail.com>
2020-01-02 17:40:21 +0800

    button add

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app justify-center>
+  <v-app justify="center" align="center">
     <v-container fill-height>
       <v-card
         bg
@@ -19,19 +19,24 @@
         <v-card-subtitle class="pb-0">Xanonymous</v-card-subtitle>
 
         <v-card-text class="text--primary">
-          <div>按下按鈕，神奇的事情將會發生。</div>
+          <div class="my-4" style="font-size:1.7em">
+            按下按鈕，神奇的事情將會發生。
+          </div>
 
           <div>純屬娛樂，請勿自行嘗試。</div>
         </v-card-text>
 
-        <v-card-actions>
-          <v-btn color="orange" text>
-            Share
-          </v-btn>
-
-          <v-btn color="orange" text>
-            Explore
-          </v-btn>
+        <v-card-actions justify="center" align="center">
+          <v-btn
+            block
+            align="center"
+            justify-center
+            center
+            x-large
+            color="success"
+            dark
+            >ATTACK NOW!!</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-container>
